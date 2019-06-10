320906e3af3bbf69004b6d82e5770c81c37665de
Dionuta (Tay) Burton <diounta@gmail.com>
2019-06-09 20:30:27 -0700

    Add files via upload

---
+++ intro_to_pyton.py
@@ -0,0 +1,21 @@
+#concatenation in python
+print("Hello World " + "My Name Is Dionuta")
+example_string = "Dionuta is going to rich"
+print(example_string)
+example_interger = 9 # or any whole number
+print(example_interger)
+example_float = 12.5 #numbers with decimals 
+print(example_float)
+example_boolean = True # in python booleans are capitalized (True or False)
+print(example_boolean) 
+example_multi_line_string = """ 
+this
+is 
+a multi line
+sting """
+print(example_multi_line_string)
+name,age,sex = 'Tay', 24,'Male'#Python allows you to assign more than on variable on one line
+print("Name:", name , "Age:" , age, "Sex:" , sex) 
+example_list = ["Hello", 123, True] #arrays 
+print(example_list)
+print(example_list[0]) #accessing arrays
